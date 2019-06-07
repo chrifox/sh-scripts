883d790856a6984cb1119b55bb3a56e19ae6c5c8
Chris Fox <chrisf@flixpremiere.com>
2019-06-07 12:34:39 +0100

    java, android move out to bp

--- android.sh
+++
@@ -1,8 +0,0 @@
-# $SCRIPTS/android.sh
-
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-export PATH=$PATH:$ANDROID_HOME/platform-tools/
