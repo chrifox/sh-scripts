4c9290b8a3f59a1f331b799ad8021edea41115e4
Chris Fox <chrisf@flixpremiere.com>
2019-06-19 16:17:00 +0100

    begin triple color flags

--- flags.sh
+++ flags.sh
@@ -17,6 +17,10 @@ country=${1}
 
 # POL
 if [[ $country == "POL" ]]; then hBiColor $cols $rows 255 1; fi
+# FRA
+if [[ $country == "FRA" ]]; then vTriColor $cols $rows 4 255 1; fi
+# ITA
+if [[ $country == "ITA" ]]; then vTriColor $cols $rows 28 255 1; fi
 }
 
 hBiColor() {
@@ -44,6 +48,25 @@ for (( row=0; row<$rows; row++ )); do
  for (( col=0; col<$cols; col++ )); do
   if [[ $col < $(($cols / 2)) ]]; then printf "$(tput setab $leftColor)"
   else printf "$(tput setab $rightColor)"
+  fi
+  printf " "
+ done
+ printf "$(tput sgr0)\n"
+done
+}
+
+vTriColor() {
+cols=$1
+rows=$2
+firstColor=$3
+secondColor=$4
+thirdColor=$5
+for (( row=0; row<$rows; row++ )); do
+ for (( col=0; col<$cols; col++ )); do
+  oneThird=$(($cols / 3))
+  if [[ $col < $oneThird ]]; then printf "$(tput setab $firstColor)"
+  elif [[ $col > $(($cols - $oneThird - 1)) ]]; then printf "$(tput setab $thirdColor)"
+  else printf "$(tput setab $secondColor)"
   fi
   printf " "
  done
