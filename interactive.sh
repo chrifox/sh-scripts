b5e6702da5f7f6dc5652c0f9e1d9f96d0bf0c478
Chris Fox <chrisf@flixpremiere.com>
2019-07-08 17:59:48 +0100

    select menu example

--- interactive.sh
+++ interactive.sh
@@ -27,3 +27,26 @@ done
 
 exit 0
 }
+
+menu() {
+title="Select example"
+prompt="Pick an option:"
+options=("A" "B" "C")
+
+echo "$title"
+PS3="$prompt "
+select opt in "${options[@]}" "Quit"; do 
+
+    case "$REPLY" in
+
+    1 ) echo "You picked $opt which is option $REPLY";;
+    2 ) echo "You picked $opt which is option $REPLY";;
+    3 ) echo "You picked $opt which is option $REPLY";;
+
+    $(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
+    *) echo "Invalid option. Try another one.";continue;;
+
+    esac
+
+done
+}
