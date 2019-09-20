e77eeb65889fdd99f98d836f81f69cde5ecc6c74
Chris Fox <confoxt@gmail.com>
2019-09-20 17:44:56 +0100

    fix config

--- config/config.sh
+++ config/config.sh
@@ -2,14 +2,12 @@
 
 # Configure which scripts you want to use in here
 
-pwd
+source ../.git_completion.sh
 
-source .git_completion.sh
-
-source aliases.sh
-source grep.sh
-source helpers.sh
-source loaders.sh
+source ../aliases.sh
+source ../grep.sh
+source ../helpers.sh
+source ../loaders.sh
 
 # # Uncomment below to load all scripts
 # for file in ../*.sh; do
