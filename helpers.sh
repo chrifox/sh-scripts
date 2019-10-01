06e39d4d764442097dbd04b5398f6d5ad50a4f32
Chris.Fox <Chris.Fox@ffx.co.uk>
2019-10-01 16:33:18 +0100

    update helpers

--- helpers.sh
+++ helpers.sh
@@ -2,9 +2,9 @@
 
 # super list
 sls() {
-dir=$1
+dir=${1:-"."}
 for item in $dir/*; do
- [[ -d $item ]] && echo "$(tput setaf 56) DIR: $(tput sgr0) ${item}" || echo "$(tput setaf 222) FILE: $(tput sgr0) ${item}"
+ [[ -d $item ]] && echo "$(tput setaf 46) DIR: $(tput sgr0) ${item}" || echo "$(tput setaf 51) FILE: $(tput sgr0) ${item}"
 done
 }
 
