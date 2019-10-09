075e4e6dcd5a7fff2d1ea1e00b52f6f9f28849fc
Chris Fox <confoxt@gmail.com>
2019-10-09 14:08:24 +0100

    more readable PS1

--- ps1.sh
+++ ps1.sh
@@ -2,5 +2,7 @@
 # default
 # \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$
 # custom
-export PS1="\[\e]0;\u:\W\a\]\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$"
-
+USERNAME="$(tput setaf 10)$(tput bold)\u$(tput sgr0)"
+WD="$(tput setaf 12)$(tput bold)\W$(tput sgr0)"
+ICON="$(tput setaf 3)\$$(tput sgr0)"
+export PS1="${USERNAME}:${WD}${ICON}"
