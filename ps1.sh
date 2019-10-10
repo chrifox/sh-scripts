8d71281fbfff85c91aa3998709f3d87d44fe0b41
Chris Fox <confoxt@gmail.com>
2019-10-10 19:09:54 +0100

    fix PS1 using ASCII escape codes

--- ps1.sh
+++ ps1.sh
@@ -1,10 +1,41 @@
 # Edit PS1
-# default
-# \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$
-# custom
-USERNAME="$(tput setaf 10)$(tput bold)\u$(tput sgr0)"
-WD="$(tput setaf 12)$(tput bold)\W$(tput sgr0)"
+
+## Docs (brief)
+## Set graphics mode
+# Esc[Value;...;Valuem
+
+# Text attributes
+# 0	All attributes off
+# 1	Bold on
+# 4	Underscore (on monochrome display adapter only)
+# 5	Blink on
+# 7	Reverse video on
+# 8	Concealed on
+  
+# Foreground colors
+# 30	Black
+# 31	Red
+# 32	Green
+# 33	Yellow
+# 34	Blue
+# 35	Magenta
+# 36	Cyan
+# 37	White
+  
+# Background colors
+# 40	Black
+# 41	Red
+# 42	Green
+# 43	Yellow
+# 44	Blue
+# 45	Magenta
+# 46	Cyan
+# 47	White
+
+## emoji
 # 💀
 # 💻
-ICON="$(tput setaf 3)\$$(tput sgr0)"
-export PS1="${USERNAME} 💻 ${WD}${ICON}"
+
+## custom
+export PS1="💻 \[\033[01;34m\]\w\n\[\033[32m\]\u\[\033[33m\]\$ \[\033[00m\]"
+
