1a4757708b0217452028c0b3bf66cb8e738762a1
Chris Fox <confoxt@gmail.com>
2019-10-18 16:45:06 +0100

    tilemap of ints

--- cpp/gen.cpp
+++ cpp/gen.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
-#include <list>
 using namespace std;
-
-int main(void)
-{
-string name;
-cout << "What's your name?\n";
-cin >> name;
-cout << "Hi " << name << "\n";
-
-return 0;
-};
 
 class Tile {
 public:
-int x;
-int y;
+//	int x;
+//	int y;
+//	int type;
+	Tile(int x, int y, int type) {
+		x = x;
+		y = y;
+		type = type;
+	}
 };
 
 class Grid {
 public:
-list <Tile> tiles;
+	Grid(int width, int height) {
+		int tiles[height][width];
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				tiles[y][x] = x;
+			}
+		}
+	}
 };
+
+int main(void) {
+int width, height;
+cout << "Enter width: ";
+cin >> width;
+cout << "Enter height: ";
+cin >> height;
+cout << "Width: " << width << "\nHeight: " << height << "\n";
+
+Grid map(width, height);
+cout << "Your Map: \n";
+for (int y = 0; y < height; y++) {
+	for (int x = 0; x < width; x++) {
+		cout << "|" << y << "," << x << "|";
+	}
+	cout << "\n";
+}
+
+return 0;
+};
+
