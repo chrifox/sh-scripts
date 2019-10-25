d4a1ff12e988b7814b0ef34e066813cbf545599d
Chris Fox <confoxt@gmail.com>
2019-10-25 10:34:53 +0100

    ignore config

---
+++ config/config.example.sh
@@ -0,0 +1,20 @@
+# $SCRIPTS/config.sh
+
+# 1. rename this file to config.sh
+# 2. configure which scripts you want to use in here
+
+# uncomment below as desired
+# SD=$SCRIPTS
+# source $SD/.git_completion.sh
+# source $SD/ps1.sh
+# source $SD/aliases.sh
+# source $SD/colors.sh
+# source $SD/grep.sh
+# source $SD/helpers.sh
+# source $SD/loaders.sh
+# source $SD/textEdit.sh
+
+# uncomment below to load all scripts
+# for file in $SD/*.sh; do
+# source $file
+# done
--- config/config.sh
+++ config/config.sh
@@ -1,10 +1,8 @@
 # $SCRIPTS/config.sh
-# Configure which scripts you want to use in here
 
 SD=$SCRIPTS
 
 source $SD/.git_completion.sh
-
 source $SD/ps1.sh
 source $SD/aliases.sh
 source $SD/colors.sh
@@ -12,8 +10,3 @@ source $SD/grep.sh
 source $SD/helpers.sh
 source $SD/loaders.sh
 source $SD/textEdit.sh
-
-# # Uncomment below to load all scripts
-# for file in $SD/*.sh; do
-# source $file
-# done
