8f2f5e73573d8f8c6e66633237fdf69d74ca9e5b
Chris Fox <confoxt@gmail.com>
2020-01-28 16:12:08 +0000

    cpp thinning

--- cpp/demo.cpp
+++ cpp/demo.cpp
@@ -3,9 +3,41 @@ using namespace std;
 
 int main(void)
 {
-	string name;
-	cout << "Enter your name: ";
-	cin >> name;
-	cout << "Hi " << name << "!\n";
-	return 0;
+// printing to console
+cout << "~~~ Hello! Welcome to C++ ~~~\n\n";
+
+// variables
+cout << "VARIABLES\n\n";
+
+int myInt = 10;
+cout << "Integer (int) eg: " << myInt << "\n";
+
+double myDouble = 7.25;
+cout << "Double (double) eg: " << myDouble << "\n";
+
+char myChar = 'C';
+cout << "Character (char) eg: '" << myChar << "'\n";
+
+string myString = "Strings use double quotes";
+cout << "String (string) eg: \"" << myString << "\"\n";
+
+bool myBool = true;
+cout << "Boolean (bool) eg: " << myBool << "\n";
+
+// declare multiple variables
+int x = 1, y = 2, z = 3;
+cout << "Print multiple vars: " << x << y << z << "\n";
+cout << "Calc with vars inline: " << x + y + z << "\n";
+
+cout << "\n";
+
+// user input
+cout << "User Input\n\n";
+
+int emptyInt;
+cout << "Choose a number: ";
+cin >> emptyInt;
+cout << "You chose the number " << emptyInt << "\n";
+
+return 0;
 }
