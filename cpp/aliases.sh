63c5c2e19893b6363eeabebd80479e205b366e26
Chris Fox <confoxt@gmail.com>
2020-01-29 12:12:19 +0000

    add docs/aliases for cpp

---
+++ cpp/aliases.sh
@@ -0,0 +1,7 @@
+# cpp aliases
+
+alias cppalias="cat $SD/cpp/aliases.sh"
+
+# sfml
+# alias cppcompile="g++ -c <file.cpp>"
+# alias cpplink="g++ <file.o> -o <output-name> -lsfml-graphics -lsfml-window -lsfml-system"
