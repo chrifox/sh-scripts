4a4483e6a3417c50cbb54a58eeb654cafac87c70
Chris Fox <confoxt@gmail.com>
2020-01-29 15:05:41 +0000

    update demo

--- cpp/demo.cpp
+++ cpp/demo.cpp
@@ -4,40 +4,40 @@ using namespace std;
 int main(void)
 {
 // printing to console
-cout << "~~~ Hello! Welcome to C++ ~~~\n\n";
+cout << "~~~ Hello! Welcome to C++ ~~~" << endl << "\n";
 
 // variables
-cout << "VARIABLES\n\n";
+cout << "VARIABLES" << endl << "\n";
 
 int myInt = 10;
-cout << "Integer (int) eg: " << myInt << "\n";
+cout << "Integer (int) eg: " << myInt << endl;
 
 double myDouble = 7.25;
-cout << "Double (double) eg: " << myDouble << "\n";
+cout << "Double (double) eg: " << myDouble << endl;
 
 char myChar = 'C';
-cout << "Character (char) eg: '" << myChar << "'\n";
+cout << "Character (char) eg: '" << myChar << endl;
 
 string myString = "Strings use double quotes";
-cout << "String (string) eg: \"" << myString << "\"\n";
+cout << "String (string) eg: \"" << myString << "\"" << endl;
 
 bool myBool = true;
-cout << "Boolean (bool) eg: " << myBool << "\n";
+cout << "Boolean (bool) eg: " << std::boolalpha << myBool << endl;
 
 // declare multiple variables
 int x = 1, y = 2, z = 3;
-cout << "Print multiple vars: " << x << y << z << "\n";
-cout << "Calc with vars inline: " << x + y + z << "\n";
+cout << "Print multiple vars: " << x << y << z << endl;
+cout << "Calc with vars inline: " << x + y + z << endl;
 
-cout << "\n";
+cout << endl;
 
 // user input
-cout << "User Input\n\n";
+cout << "USER INPUT" << endl << "\n";
 
 int emptyInt;
 cout << "Choose a number: ";
 cin >> emptyInt;
-cout << "You chose the number " << emptyInt << "\n";
+cout << "You chose the number " << emptyInt << endl;
 
 return 0;
 }
