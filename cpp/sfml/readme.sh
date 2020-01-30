f71f0a50a7261aca2164cb0500f4bb174de2ddba
Chris Fox <confoxt@gmail.com>
2020-01-30 09:24:53 +0000

    refactor

--- cpp/sfml/readme.sh
+++ cpp/sfml/readme.sh
@@ -1,7 +1,4 @@
-# cpp aliases
+# sfml aliases
 
-alias cppalias="cat $SD/cpp/aliases.sh"
-
-# sfml
 # alias cppcompile="g++ -c <file.cpp>"
 # alias cpplink="g++ <file.o> -o <output-name> -lsfml-graphics -lsfml-window -lsfml-system"
