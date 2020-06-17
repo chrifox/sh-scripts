715438eefddb2901bd33191826c7c3b1e4f15ba9
chrifox <confoxt@gmail.com>
2020-06-17 12:01:56 +0100

    remove git warnings from console startup

--- aliases.sh
+++ aliases.sh
@@ -12,12 +12,16 @@ alias gpu="git push"
 alias glog="git log --graph --all --oneline --decorate"
 alias gpr="git remote prune origin"
 alias gprdr="git remote prune origin --dry-run"
+
 # list all merged remote branches
-alias gbremote="for branch in `git branch -r --merged | grep -v HEAD`; do echo -e `git show --format="%ci %cr %an" $branch | head -n 1` \\t$branch; done | sort -r"
+# alias gbremote="for branch in `git branch -r --merged | grep -v HEAD`; do echo -e `git show --format="%ci %cr %an" $branch | head -n 1` \\t$branch; done | sort -r"
+
 # list all unmerged remote branches
-alias gburemote="for branch in `git branch -r --no-merged | grep -v HEAD`; do echo -e `git show --format="%ci %cr %an" $branch | head -n 1` \\t$branch; done | sort -r"
+# alias gburemote="for branch in `git branch -r --no-merged | grep -v HEAD`; do echo -e `git show --format="%ci %cr %an" $branch | head -n 1` \\t$branch; done | sort -r"
+
 # delete remote branch
-alias gbremoted="git push origin --delete branch-name" 
+# alias gbremoted="git push origin --delete branch-name" 
+
 # yarn
 alias ys="yarn start"
 alias yt="yarn test"
